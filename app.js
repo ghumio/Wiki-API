5799b4e0b11d075af255c038bc1bf55e30995d7a
Prakash Shrestha <ghumio11@gmail.com>
2024-05-15 21:25:41 +0545

    final commit

--- app.js
+++ app.js
@@ -23,7 +23,98 @@ const articleSchema = {
 
 const Article = mongoose.model("Article", articleSchema);
 
-//TODO
+app.route("/articles")
+
+.get(function(req, res){
+  Article.find()
+      .then(foundArticles => {
+          res.send(foundArticles);
+      })
+      .catch(err => {
+          res.send(err);
+      });
+})
+
+.post(function(req, res){
+  const newArticle = new Article({
+      title: req.body.title,
+      content: req.body.content
+  });
+  newArticle.save()
+      .then(() => {
+          res.send("Successfully added a new article.");
+      })
+      .catch(err => {
+          res.send(err);
+      });
+})
+
+.delete(function(req, res){
+  Article.deleteMany()
+      .then(() => {
+          res.send("Successfully deleted all articles.");
+      })
+      .catch(err => {
+          res.send(err);
+      });
+})
+
+//request for specific article
+
+app.route("/articles/:articleTitle")
+.get(function(req, res){
+  Article.findOne({title: req.params.articleTitle})
+      .then(foundArticle => {
+          if(foundArticle){
+              res.send(foundArticle);
+          } else {
+              res.send("No articles matching that title was found.");
+          }
+      })
+      .catch(err => {
+          res.send(err);
+      });
+})
+
+.put(function(req, res){
+  Article.update(
+      {title: req.params.articleTitle},
+      {title: req.body.title, content: req.body.content},
+      {overwrite: true}
+  )
+      .then(() => {
+          res.send("Successfully updated article.");
+      })
+      .catch(err => {
+          res.send(err);
+      });
+})
+
+.patch(function(req, res){
+  Article.update(
+      {title: req.params.articleTitle},
+      {$set: req.body}
+  )
+      .then(() => {
+          res.send("Successfully updated article.");
+      })
+      .catch(err => {
+          res.send(err);
+      });
+})
+
+.delete(function(req, res){
+  Article.deleteOne({title: req.params.articleTitle})
+      .then(() => {
+          res.send("Successfully deleted the corresponding article.");
+      })
+      .catch(err => {
+          res.send(err);
+      });
+})
+
+
+
 
 app.listen(3000, function() {
   console.log("Server started on port 3000");
